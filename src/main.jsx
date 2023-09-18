ce433565c9c3c5271db3aca4a0c56701dedd24d2
Guruvignesh M <vigneshguru274@gmail.com>
2023-09-18 17:14:18 +0530

    add the toast container components

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ThemeProvider from './utils/ThemeContext';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import ThemeProvider from "./utils/ThemeContext";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
         <App />
+        <ToastContainer />
       </ThemeProvider>
     </Router>
   </React.StrictMode>
